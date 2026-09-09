cb43ad4f4535d082b01b705427c19dc1471198a1
Daniela <danielarodrigues2po@gmail.com>
2026-09-08 21:34:59 -0300

    create troup card class implemented

---
+++ scripts/buttons/CreateTroup.py
@@ -0,0 +1,18 @@
+import pygame as pg
+
+class CreateTroup(pg.sprite.Sprite):
+
+    def __init__(self, x, y, color):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((32,32))
+        self.image.fill(color)
+        self.x = x
+        self.y = y
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
