9a585b06faa62a276840abb8d55fc271e9913eda
danielarodrigues2 <danielarodrigues2po@gmail.com>
2026-09-14 08:25:37 -0300

    Lifer bar update, and implementation life bar in towers

--- scripts/towers/Tower.py
+++ scripts/towers/Tower.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from scripts.utils.LifeBar import LifeBar
 
 class Tower(pg.sprite.Sprite):
 
@@ -12,12 +13,15 @@ class Tower(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y) 
         self.money = 0
+        self.life = 500
+        self.lifeBar = LifeBar(70, self.x, self.y - 70, self.life)
 
     def update(self):
-        pass
+        self.lifeBar.update(self.x, self.life)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+        self.lifeBar.draw(screen)
 
     def gain_money(self, money):
         self.money += money
--- scripts/utils/LifeBar.py
+++ scripts/utils/LifeBar.py
@@ -3,8 +3,9 @@ import pygame as pg
 class LifeBar(pg.sprite.Sprite):
     def __init__(self, size, x , y, life):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((size, 10))
-        self.image.fill = (0, 255, 0)
+        self.size = size
+        self.image = pg.Surface((self.size, 10))
+        self.image.fill((0, 255, 0))
         self.life = life
         self.x = x
         self.y = y
@@ -12,8 +13,9 @@ class LifeBar(pg.sprite.Sprite):
         self.rect.center = (self.x, self.y)
 
     def update(self, x, life):
-        self.image = pg.Surface((life, 10))
-        self.image.fill = (0, 255, 0)
+        size = (self.size * life) / self.life
+        self.image = pg.Surface((size, 10))
+        self.image.fill((0, 255, 0))
         self.rect = self.image.get_rect()
         self.rect.center(x, self.y)
 
